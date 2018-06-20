5d65edfc32362a53df854a1413de5af6836daa0f
Riccardo Bonesi <riccardo.bonesi@gmail.com>
2018-06-20 16:35:12 +0200

    uuuu

--- bonesi_analysis.py
+++ bonesi_analysis.py
@@ -7,6 +7,33 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from wordcloud import WordCloud
 # from sklearn.metrics import roc_curve, auc, classification_report
+from nltk.tag.stanford import StanfordPOSTagger as POS_Tag
+from nltk import word_tokenize
+from sklearn.externals import joblib
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+
+from sklearn.externals import joblib
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import SGDClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import svm
+
+from scipy.sparse import hstack
+
+
+import os
+java_path = "C:/Program Files/Java/jdk1.8.0_161/bin/java.exe"
+os.environ['JAVAHOME'] = java_path
 
 
 
@@ -26,16 +53,16 @@ def exploratory_data_analysis(df):
     print("Median Score: ", np.median(df.score))
 
 
-    # creo un nuovo attributo Sentiment
+    # creo un nuovo attributo opinion
     # negative = review(1-3), positive=(4-5)
-    df.ix[df.score > 3, 'Sentiment'] = "POSITIVE"
-    df.ix[df.score <= 3, 'Sentiment'] = "NEGATIVE"
+    df.ix[df.score > 3, 'opinion'] = "positive"
+    df.ix[df.score <= 3, 'opinion'] = "negative"
 
 
-    ## Sentiment distribution
+    ## opinion distribution
     # ax = plt.axes()
-    # sns.countplot(df.Sentiment, ax=ax)
-    # ax.set_title('Sentiment Positive vs Negative Distribution')
+    # sns.countplot(df.opinion, ax=ax)
+    # ax.set_title('opinion Positive vs Negative Distribution')
     # plt.show()
 
     ## userid distribution
@@ -50,27 +77,27 @@ def exploratory_data_analysis(df):
     # ax.set_title('ProductId Distribution')
     # plt.show()
 
-    print("Proportion of positive review:", len(df[df.Sentiment == "POSITIVE"]) / len(df))
-    print("Proportion of positive review:", len(df[df.Sentiment == "NEGATIVE"]) / len(df))
+    print("Proportion of positive review:", len(df[df.opinion == "positive"]) / len(df))
+    print("Proportion of positive review:", len(df[df.opinion == "negative"]) / len(df))
     # 77% of the fine food reviews are considered as positive
     # and 23% of them are considered as negative.
 
     reviews = df.text.values
-    labels = df.Sentiment.values
+    labels = df.opinion.values
 
     ### TEXT REVIEWS ###
 
-    if df.Sentiment[1] == "POSITIVE":
-        print("POSITIVE" + "\t" + reviews[1][:90] + "...")
+    if df.opinion[1] == "positive":
+        print("positive" + "\t" + reviews[1][:90] + "...")
     else:
-        print("NEGATIVE" + "\t " + reviews[1][:90] + "...")
+        print("negative" + "\t " + reviews[1][:90] + "...")
 
 
     ### Exploratory Visualization ###
     # mi creo un vocabolario di parole positive e negative
 
-    positive_reviews = [reviews[i] for i in range(len(reviews)) if labels[i] == "POSITIVE"]
-    negative_reviews = [reviews[i] for i in range(len(reviews)) if labels[i] == "NEGATIVE"]
+    positive_reviews = [reviews[i] for i in range(len(reviews)) if labels[i] == "positive"]
+    negative_reviews = [reviews[i] for i in range(len(reviews)) if labels[i] == "negative"]
 
     ### positive
     cnt_positve = Counter()
@@ -110,7 +137,7 @@ def exploratory_data_analysis(df):
 
 
 
-    #### POSITIVE WORDCLOUD #####
+    #### positive WORDCLOUD #####
     wordcloud = WordCloud()
     wordcloud.generate_from_frequencies(frequencies=positive_dict)
 
@@ -128,7 +155,7 @@ def exploratory_data_analysis(df):
 
 
 
-    #### NEGATIVE WORDCLOUD #####
+    #### negative WORDCLOUD #####
     wordcloud = WordCloud()
     wordcloud.generate_from_frequencies(frequencies=negative_dict)
     plt.figure(figsize=(10, 10))
@@ -199,12 +226,20 @@ def vocabulary_reduction(reviews, labels, min_freq=10, polarity_cut_off=0.1):
 def data_preprocessing(df):
 
     reviews = df.text.values
-    labels = np.array([1 if s == "POSITIVE" else 0 for s in df.Sentiment.values])
+    labels = np.array([1 if s == "positive" else 0 for s in df.opinion.values])
 
     ### remove punctuations
     reviews_cleaned = []
     for i in range(len(reviews)):
         reviews_cleaned.append(''.join([c.lower() for c in reviews[i] if c not in punctuation]))
+
+    # for i in range(len(reviews)):
+    #     for c in reviews[i]:
+    #         if c not in punctuation:
+    #             reviews_cleaned.append(''.join(c.lower()))
+    #         else:
+    #             reviews_cleaned.append(' '.join)
+
 
     print("Before: ", reviews[0])
     print("")
@@ -242,13 +277,170 @@ def data_preprocessing(df):
 
 
 
-
-
-
-
-
-
     print("END PREPROCESSING")
+
+
+    ############################    ASPECT BASED   ################################################################
+
+#Selecting only 20 most common aspect.
+def get_most_common_aspect(opinion_list):
+    opinion = []
+    for inner_list in opinion_list:
+        for _dict in inner_list:
+            for key in _dict:
+                opinion.append(key)
+    most_common_aspect = [k for k, v in nltk.FreqDist(opinion).most_common(20)]
+    return most_common_aspect
+
+
+#To tag using stanford pos tagger
+def posTag(review, stanford_tag):
+    tagged_text_list=[]
+    for text in review:
+        tagged_text_list.append(stanford_tag.tag(word_tokenize(text)))
+    return tagged_text_list
+
+
+def filterTag(tagged_review):
+    final_text_list=[]
+    for text_list in tagged_review:
+        final_text=[]
+        for word,tag in text_list:
+            if tag in ['NN','NNS','NNP','NNPS','RB','RBR','RBS','JJ','JJR','JJS','VB','VBD','VBG','VBN','VBP','VBZ']:
+                final_text.append(word)
+        final_text_list.append(' '.join(final_text))
+    return final_text_list
+
+
+
+def get_dict_aspect(y,most_common_aspect):
+    position=[]
+    for innerlist in y:
+        position.append([i for i, j in enumerate(innerlist) if j == 1])
+    sorted_common=sorted(most_common_aspect)
+    dict_aspect=[]
+    for innerlist in position:
+        inner_dict={}
+        for word in sorted_common:
+            if sorted_common.index(word) in innerlist:
+                inner_dict[word]= 5
+            else:
+                inner_dict[word]=0
+        dict_aspect.append(inner_dict)
+    return dict_aspect
+
+
+
+#generate data frame
+def get_data_frame(text_list,opinion_list,most_common_aspect):
+    data = {'Review': text_list}
+    new_df = pd.DataFrame(data)
+    for inner_list in opinion_list:
+        for _dict in inner_list:
+            for key in _dict:
+                if key in most_common_aspect:
+                    new_df.loc[opinion_list.index(inner_list), key] = _dict[key]
+    return new_df
+
+
+#generate data frame for aspect extraction task
+def get_aspect_data_frame(new_df, most_common_aspect):
+    for common_aspect in most_common_aspect:
+        new_df[common_aspect]=new_df[common_aspect].replace(['positive','negative','neutral','conflict'],[1,1,1,1])
+    new_df = new_df.fillna(0)
+    return new_df
+
+
+
+
+
+
+
+
+def aspect_based(df_train, my_df_test):
+
+    ## text_list = text
+    ## opinion_list = opinion
+
+
+    # For stanford POS Tagger
+    home = r'Utils\stanford-postagger-full-2017-06-09'
+
+    _path_to_model = home + '/models/english-bidirectional-distsim.tagger'
+    _path_to_jar = home + '/stanford-postagger.jar'
+    stanford_tag = POS_Tag(model_filename=_path_to_model, path_to_jar=_path_to_jar)
+
+
+    train_text_list = df_train.text.values
+    train_opinion_list = df_train.opinion.values
+    most_common_aspect = get_most_common_aspect(train_opinion_list)
+
+    print("start tagging...")
+
+    # This takes time to tag. Already tagged and saved. So, loading file ...
+    # tagged_text_list_train=posTag(text_list, stanford_tag)
+    # joblib.dump(tagged_text_list_train, 'tagged_text_list_train.pkl')
+    tagged_text_list_train = joblib.load('tagged_text_list_train.pkl')
+
+    final_train_text_list = filterTag(tagged_text_list_train)
+
+
+    df_train = get_data_frame(final_train_text_list, train_opinion_list, most_common_aspect)
+    df_train_aspect = get_aspect_data_frame(df_train, most_common_aspect)
+    df_train_aspect = df_train_aspect.reindex_axis(sorted(df_train_aspect.columns), axis=1)
+
+    # Similar for test list
+    test_text_list = my_df_test.text.values
+    test_opinion_list = my_df_test.opinion.values
+
+    tagged_text_list_test=posTag(test_text_list)
+    joblib.dump(tagged_text_list_test, 'tagged_text_list_test.pkl')
+    # tagged_text_list_test = joblib.load('tagged_text_list_test.pkl')
+
+
+    final_test_text_list = filterTag(tagged_text_list_test)
+    df_test = get_data_frame(final_test_text_list, test_opinion_list, most_common_aspect)
+    df_test_aspect = get_aspect_data_frame(df_test, most_common_aspect)
+    df_test_aspect = df_test_aspect.reindex_axis(sorted(df_test_aspect.columns), axis=1)
+
+
+    # Sort the data frame according to aspect's name and separate data(X) and target(y)
+    # df_train_aspect = df_train_aspect.sample(frac=1).reset_index(drop=True) #For randoming
+    X_train = df_train_aspect.Review
+    y_train = df_train_aspect.drop('Review', 1)
+
+    # df_test_aspect = df_test_aspect.sample(frac=1).reset_index(drop=True) #For randoming
+    X_test = df_test_aspect.Review
+    y_test = df_test_aspect.drop('Review', 1)
+
+
+    # Change y_train to numpy array
+    y_train = np.asarray(y_train, dtype=np.int64)
+    y_test = np.asarray(y_test, dtype=np.int64)
+
+    # Generate word vecotors using CountVectorizer
+    vect = CountVectorizer(max_df=1.0, stop_words='english')
+    X_train_dtm = vect.fit_transform(X_train)
+    X_test_dtm = vect.transform(X_test)
+
+    # Create various models. These are multi-label models.
+    nb_classif = OneVsRestClassifier(MultinomialNB()).fit(X_train_dtm, y_train)
+    C = 1.0  # SVregularization parameter
+    svc = OneVsRestClassifier(svm.SVC(kernel='linear', C=C)).fit(X_train_dtm, y_train)
+    lin_svc = OneVsRestClassifier(svm.LinearSVC(C=C)).fit(X_train_dtm, y_train)
+    sgd = OneVsRestClassifier(SGDClassifier()).fit(X_train_dtm, y_train)
+
+
+
+
+
+
+
+
+
+
+
+    print("END ASPECT-BASED")
 
 
 
@@ -262,3 +454,6 @@ if __name__ == "__main__":
     df = pd.read_csv("Dataset/food.tsv", sep="\t", encoding='latin-1')
     exploratory_data_analysis(df)
     data_preprocessing(df)
+    df_train = df[1:200]
+    df_test = df[201:250]
+    aspect_based(df_train, df_test)
