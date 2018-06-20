5c46488da7b07cfd0100745d87c1b0f1c1cdc94d
Riccardo Bonesi <riccardo.bonesi@gmail.com>
2018-06-20 17:09:17 +0200

    Merge branch 'master' of https://gitlab.com/RiccardoBonesi/AmazonReview
    # Conflicts:
    #	bonesi_analysis.py

--- bonesi_analysis.py
+++ bonesi_analysis.py
@@ -30,6 +30,9 @@ from sklearn import svm
 
 from scipy.sparse import hstack
 
+from gensim import corpora
+import pickle
+import gensim
 
 import os
 java_path = "C:/Program Files/Java/jdk1.8.0_161/bin/java.exe"
@@ -277,6 +280,7 @@ def data_preprocessing(df):
 
 
 
+
     print("END PREPROCESSING")
 
 
@@ -444,8 +448,108 @@ def aspect_based(df_train, my_df_test):
 
 
 
+######################################################
+######################################################
+######################################################
+######################################################
 
 
+import spacy
+# spacy.load('en')
+from spacy.lang.en import English
+parser = English()
+
+import nltk
+
+nltk.download('wordnet')
+from nltk.corpus import wordnet as wn
+
+
+def get_lemma(word):
+    lemma = wn.morphy(word)
+    if lemma is None:
+        return word
+    else:
+        return lemma
+
+
+from nltk.stem.wordnet import WordNetLemmatizer
+
+
+def get_lemma2(word):
+    return WordNetLemmatizer().lemmatize(word)
+def tokenize(text):
+    lda_tokens = []
+    tokens = parser(text)
+    for token in tokens:
+        if token.orth_.isspace():
+            continue
+        elif token.like_url:
+            lda_tokens.append('URL')
+        elif token.orth_.startswith('@'):
+            lda_tokens.append('SCREEN_NAME')
+        else:
+            lda_tokens.append(token.lower_)
+    return lda_tokens
+
+
+
+nltk.download('stopwords')
+en_stop = set(nltk.corpus.stopwords.words('english'))
+
+
+
+def prepare_text_for_lda(text):
+    tokens = tokenize(text)
+    tokens = [token for token in tokens if len(token) > 4]
+    tokens = [token for token in tokens if token not in en_stop]
+    tokens = [get_lemma(token) for token in tokens]
+    return tokens
+
+
+
+# import random
+# text_data = []
+# with open('dataset.csv') as f:
+#     for line in f:
+#         tokens = prepare_text_for_lda(line)
+#         if random.random() > .99:
+#             print(tokens)
+#             text_data.append(tokens)
+
+
+def aspect2(df_train, df_test):
+
+    reviews = df_train.text.values
+
+    text_data = []
+    for r in reviews:
+        tokens = prepare_text_for_lda(r)
+        text_data.append(tokens)
+
+    # LDA with Gensim
+    # First, we are creating a dictionary from the data,
+    # then convert to bag-of-words corpus and save the dictionary and corpus for future use.
+    dictionary = corpora.Dictionary(text_data)
+    corpus = [dictionary.doc2bow(text) for text in text_data]
+    pickle.dump(corpus, open('corpus.pkl', 'wb'))
+    dictionary.save('dictionary.gensim')
+
+
+    # We are asking LDA to find 5 topics in the data
+    NUM_TOPICS = 20
+    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=NUM_TOPICS, id2word=dictionary, passes=15)
+    ldamodel.save('model5.gensim')
+    topics = ldamodel.print_topics(num_words=4)
+    for topic in topics:
+        print(topic)
+
+
+
+
+
+
+    print("END ASPECT2")
 
 
 
@@ -454,6 +558,7 @@ if __name__ == "__main__":
     df = pd.read_csv("Dataset/food.tsv", sep="\t", encoding='latin-1')
     exploratory_data_analysis(df)
     data_preprocessing(df)
-    df_train = df[1:200]
+    df_train = df = df.loc[df['productid'] == "B000DZFMEQ"]
     df_test = df[201:250]
-    aspect_based(df_train, df_test)
+    # aspect_based(df_train, df_test)
+    aspect2(df_train, df_test)
