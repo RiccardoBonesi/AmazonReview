757a102bc6e7c3fe8c16022cef40daca7a1fc423
luca-lorusso <arenzano02>
2018-06-20 16:35:15 +0200

    add script LDA

---
+++ luca_analysis.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+from nltk.tokenize import RegexpTokenizer
+from stop_words import get_stop_words
+from nltk.stem.porter import PorterStemmer
+from gensim import corpora, models
+import gensim
+
+tokenizer = RegexpTokenizer(r'\w+')
+
+# create English stop words list
+en_stop = get_stop_words('en')
+
+# Create p_stemmer of class PorterStemmer
+p_stemmer = PorterStemmer()
+
+# create sample documents
+df = pd.read_csv("Dataset/food.tsv", sep="\t", encoding='latin-1')
+
+
+
+# compile sample documents into a list
+doc_set = df[1:1000].text.values
+
+# list for tokenized documents in loop
+texts = []
+
+# loop through document list
+for i in doc_set:
+    # clean and tokenize document string
+    raw = i.lower()
+    tokens = tokenizer.tokenize(raw)
+
+    # remove stop words from tokens
+    stopped_tokens = [i for i in tokens if not i in en_stop]
+
+    # stem tokens
+    stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+
+    # add tokens to list
+    texts.append(stemmed_tokens)
+
+# turn our tokenized documents into a id <-> term dictionary
+dictionary = corpora.Dictionary(texts)
+
+# convert tokenized documents into a document-term matrix
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+# generate LDA model
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=2, id2word=dictionary, passes=20)
