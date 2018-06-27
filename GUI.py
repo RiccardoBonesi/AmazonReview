4805387f74dd2c6ff1fa9cf48a701490d3574342
Riccardo Bonesi <riccardo.bonesi@gmail.com>
2018-06-27 17:34:20 +0200

    prova settaggio valori

--- GUI.py
+++ GUI.py
@@ -6,8 +6,13 @@ from PyQt5.QtWidgets import QDialog, QApplication
 
 class Ui_Form(object):
     def setupUi(self, Form):
+
+        self.method = 1
+
+
         Form.setObjectName("Form")
         Form.resize(924, 409)
+
         self.label = QtWidgets.QLabel(Form)
         self.label.setGeometry(QtCore.QRect(310, 40, 321, 20))
         font = QtGui.QFont()
@@ -17,6 +22,7 @@ class Ui_Form(object):
         self.label.setFont(font)
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
+
         self.label_2 = QtWidgets.QLabel(Form)
         self.label_2.setGeometry(QtCore.QRect(20, 115, 301, 21))
         font = QtGui.QFont()
@@ -25,18 +31,25 @@ class Ui_Form(object):
         font.setWeight(75)
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
+
         self.radioButton = QtWidgets.QRadioButton(Form)
         self.radioButton.setGeometry(QtCore.QRect(30, 150, 191, 20))
         font = QtGui.QFont()
         font.setPointSize(9)
         self.radioButton.setFont(font)
         self.radioButton.setObjectName("radioButton")
+        self.radioButton.setChecked(True)
+        self.radioButton.toggled.connect(lambda: self.on_radio(1))
+
         self.radioButton_2 = QtWidgets.QRadioButton(Form)
         self.radioButton_2.setGeometry(QtCore.QRect(30, 180, 251, 20))
         font = QtGui.QFont()
         font.setPointSize(9)
         self.radioButton_2.setFont(font)
         self.radioButton_2.setObjectName("radioButton_2")
+        self.radioButton_2.toggled.connect(lambda: self.on_radio(1))
+        self.radioButton_2.toggled()
+
         self.label_3 = QtWidgets.QLabel(Form)
         self.label_3.setGeometry(QtCore.QRect(580, 115, 201, 21))
         font = QtGui.QFont()
@@ -45,6 +58,7 @@ class Ui_Form(object):
         font.setWeight(75)
         self.label_3.setFont(font)
         self.label_3.setObjectName("label_3")
+
         self.comboBox = QtWidgets.QComboBox(Form)
         self.comboBox.setGeometry(QtCore.QRect(580, 160, 251, 31))
         font = QtGui.QFont()
@@ -53,12 +67,14 @@ class Ui_Form(object):
         self.comboBox.setObjectName("comboBox")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
-        self.pushButton = QtWidgets.QPushButton(Form)
-        self.pushButton.setGeometry(QtCore.QRect(390, 320, 141, 61))
+
+        self.startButton = QtWidgets.QPushButton(Form)
+        self.startButton.setGeometry(QtCore.QRect(390, 320, 141, 61))
         font = QtGui.QFont()
         font.setPointSize(10)
-        self.pushButton.setFont(font)
-        self.pushButton.setObjectName("pushButton")
+        self.startButton.setFont(font)
+        self.startButton.setObjectName("pushButton")
+        self.startButton.clicked.connect(lambda: self.on_start(1))
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -73,7 +89,31 @@ class Ui_Form(object):
         self.label_3.setText(_translate("Form", "Select Product:"))
         self.comboBox.setItemText(0, _translate("Form", "Prodotto 1"))
         self.comboBox.setItemText(1, _translate("Form", "Prodotto 2"))
-        self.pushButton.setText(_translate("Form", "START"))
+        self.startButton.setText(_translate("Form", "START"))
+
+
+    # prendo il valore dei radio button
+    def on_radio(self, y):
+        # print("RADIO BUTTON")
+        # print(y)
+        # imposto il tipo di analisi
+        self.method = y
+
+
+    # Metodo associato al tasto START
+    def on_start(self, product):
+        print("Bottone START")
+        # print(product)
+        print("method: " + self.method)
+
+
+
+
+
+
+
+
+
 
 
 
